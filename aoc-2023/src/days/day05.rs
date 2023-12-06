4d3e6e36b02b9b831def45ceb4a33bfaf52d050e
Marton Szabo <szabo@xund.ai>
2023-12-06 18:15:49 +0100

    fix: day05

--- aoc-2023/src/days/day05.rs
+++ aoc-2023/src/days/day05.rs
@@ -19,7 +19,7 @@ pub fn a() {
     let mut indexes = HashMap::new();
     for i in 1..data.len() {
         gen_new_indexes(&mut indexes, &data[i]);
-        println!("{:?}", indexes);
+        //println!("{:?}", indexes);
         //println!("{:?}", nums);
         nums = get_next_values(nums,i, &data);
     }
@@ -28,7 +28,7 @@ pub fn a() {
 
 pub fn b() {
     let mut line_vec: Vec<String>= vec![];
-    if let Ok(lines) = read_lines("./input05-def") {
+    if let Ok(lines) = read_lines("./input05") {
         for line in lines {
             if let Ok(ip) = line {
                 line_vec.append(&mut vec![ip]);
@@ -37,14 +37,18 @@ pub fn b() {
     }
     let data = parse_input(&line_vec);
     let mut ans: u128 = 0;
-    //let seeds = get_seeds(data[0][0].to_vec());
+    //let seeds = get_seeds(&data[0][0].to_vec());
+    //println!("{:?}", seeds);
     //let mut locations = get_locations(data[data.len() - 1].to_vec());
     //locations.append(&mut (0..locations.iter().min().unwrap() - 1).collect::<Vec<u128>>());
     //locations.sort();
     //println!("{}",seeds.len());
     //println!("{}",locations.len());
-    let mut i = 0;
+    let mut i = 69000000;
     while ans == 0 {
+        if i % 100000 ==0 {
+            println!("{}",i);
+        }
         let mut calc = i + 1;
         let location = calc;
         for i in (1..data.len()).rev() {
@@ -103,6 +107,15 @@ fn get_next_values(nums: Vec<u128>, check_index: usize, data: &Vec<Vec<Vec<u128>
 }
 
 
+fn get_seeds(data: &Vec<u128>) -> Vec<u128> {
+    let mut seeds: Vec<u128> = vec![];
+    for i in (0..data.len()).step_by(2) {
+        for j in 0..data[i+1] {
+            seeds.append(&mut vec![&data[i]+j]);
+        }
+    }
+    seeds
+}
 fn seed_exists(value: u128, data: &Vec<u128>) -> bool {
     for i in (0..data.len()).step_by(2) {
         if value >= data[i] && value <= data[i] + data[i+1] {
@@ -146,9 +159,9 @@ fn get_io_min(data: Vec<Vec<u128>>) -> Vec<u128> {
 }
 
 fn gen_new_indexes(indexes: &mut HashMap<u128,u128>, data: &Vec<Vec<u128>>) {
-    println!("{:?}",data);
+    //println!("{:?}",data);
     for v in data {
-    println!("{:?}",v);
+    //println!("{:?}",v);
         for d in 0..v[2] {
             if let Some(val) = indexes.remove(&(v[1] + d)) {
                 //println!("removing old{}", v[1] + d);
