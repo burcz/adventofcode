a79b14f167857a0aba58199f3a8224ca48e170c7
burcz <burczeckley@gmail.com>
2024-12-05 17:33:33 +0100

    chore: day01

--- aoc-2024/day01/main.go
+++ aoc-2024/day01/main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+  "slices"
+)
+
+
+func abs(a int64) int64 {
+    if a >= 0 {
+        return a
+    }
+    return -a
+}
+
+
+func readLines(path string) ([]string, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
-  fmt.Println("fele")
+
+    lines, err := readLines("./input.txt")
+    check(err)
+    var l []int64
+    var r []int64
+    for lineNum := range lines {
+      leftInt, err := strconv.ParseInt(strings.Split(lines[lineNum], "   ")[0], 10, 32)
+      rightInt, err := strconv.ParseInt(strings.Split(lines[lineNum], "   ")[1], 10, 32)
+      check(err)
+      l = append(l, leftInt)
+      r = append(r, rightInt)
+    }
+    slices.Sort(l)
+    slices.Sort(r)
+
+    var suma int64
+    var sumb int64
+    for i := range l {
+      for j := range r {
+        if l[i] == r[j] {
+          sumb += l[i]
+        }
+      }
+      suma += abs(r[i] - l[i])
+
+    }
+
+    fmt.Printf("a: %d\n", suma)
+    fmt.Printf("b: %d\n", sumb)
+
+
+
+
+
 }
